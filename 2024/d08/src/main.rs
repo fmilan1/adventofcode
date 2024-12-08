5b65ad46ca81568d470950f00c3964244200f5d7
Fazekas Milan <fazekas.milan1@gmail.com>
2024-12-08 09:06:02 +0100

    2024 day 8 second *

--- 2024/d08/src/main.rs
+++ 2024/d08/src/main.rs
@@ -3,7 +3,8 @@ use std::collections::HashMap;
 fn main() {
     let file = std::fs::read_to_string("input2.txt").unwrap();
     let map: Vec<Vec<char>> = file.lines().map(|l| l.chars().collect()).collect();
-    let mut antinodes: Vec<Vec<char>> = vec![vec!['.'; map[0].len()]; map.len()];
+    let mut antinodes1: Vec<Vec<char>> = vec![vec!['.'; map[0].len()]; map.len()];
+    let mut antinodes2 = antinodes1.clone();
     let mut freqs: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
     for (i, line) in map.clone().into_iter().enumerate() {
         for (j, c) in line.into_iter().enumerate() {
@@ -24,19 +25,33 @@ fn main() {
                 let new_i: (usize, usize) = (positions[i].0 - dis.0, positions[j].0 + dis.0);
                 let new_j: (usize, usize) = (positions[i].1 - dis.1, positions[j].1 + dis.1);
                 if new_i.0 < map.len() && new_j.0 < map[0].len() {
-                    antinodes[new_i.0][new_j.0] = '#';
+                    antinodes1[new_i.0][new_j.0] = '#';
                 }
                 if new_i.1 < map.len() && new_j.1 < map[0].len() {
-                    antinodes[new_i.1][new_j.1] = '#';
+                    antinodes1[new_i.1][new_j.1] = '#';
+                }
+                for k in 0..map[0].clone().len() {
+                    let dis: (usize, usize) = (positions[j].0 - positions[i].0, positions[j].1 - positions[i].1);
+                    let new_i: (usize, usize) = (positions[i].0 - dis.0 * k, positions[j].0 + dis.0 * k);
+                    let new_j: (usize, usize) = (positions[i].1 - dis.1 * k, positions[j].1 + dis.1 * k);
+                    if new_i.0 < map.len() && new_j.0 < map[0].len() {
+                        antinodes2[new_i.0][new_j.0] = '#';
+                    }
+                    if new_i.1 < map.len() && new_j.1 < map[0].len() {
+                        antinodes2[new_i.1][new_j.1] = '#';
+                    }
                 }
             }
         }
     }
-    let mut count = 0;
-    for line in antinodes {
-        for c in line {
-            if c == '#' { count += 1; }
+    let mut count1 = 0;
+    let mut count2 = 0;
+    for i in 0..antinodes1.len() {
+        for j in 0..antinodes1[i].len() {
+            if antinodes1[i][j] == '#' { count1 += 1; }
+            if antinodes2[i][j] == '#' { count2 += 1; }
         }
     }
-    println!("{count}");
+    println!("{count1}");
+    println!("{count2}");
 }
