61c4f1a327a74e9098a5a07ff243ab1a9fa676ae
Fazekas Milán <fazekas.milan1@gmail.com>
2024-12-11 12:35:39 +0100

    2024 day 11 first *

---
+++ 2024/d11/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let file = include_str!("../input2.txt");
+    let mut stones: Vec<u64> = file.trim().split(" ").into_iter().map(|s| s.parse().unwrap()).collect();
+    for j in 0..75 {
+        let mut new_stones: Vec<u64> = Vec::new();
+        let mut i = 0;
+        while i < stones.len() {
+            if stones[i] == 0 { new_stones.push(1); }
+            else if stones[i].to_string().len() % 2 == 0 {
+                let left = &stones[i].to_string()[..stones[i].to_string().len() / 2];
+                let right = &stones[i].to_string()[stones[i].to_string().len() / 2..];
+                new_stones.push(left.parse().unwrap());
+                new_stones.push(right.parse().unwrap());
+            }
+            else { new_stones.push(stones[i] * 2024); }
+            //stones = new_stones.clone();
+            i += 1;
+        }
+        stones = new_stones.clone();
+    }
+    println!("{}", stones.len());
+}
