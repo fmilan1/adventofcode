11cf100f7888213d25ecb445abb8ebeb88a8c69d
Fazekas Milan <fazekas.milan1@gmail.com>
2024-12-11 17:18:38 +0100

    fix 2024 day 11 first *

--- 2024/d11/src/main.rs
+++ 2024/d11/src/main.rs
@@ -1,19 +1,17 @@
 fn main() {
     let file = include_str!("../input2.txt");
-    let mut stones: Vec<u64> = file.trim().split(" ").into_iter().map(|s| s.parse().unwrap()).collect();
-    for _j in 0..25 {
-        let mut new_stones: Vec<u64> = Vec::new();
-        let mut i = 0;
-        while i < stones.len() {
-            if stones[i] == 0 { new_stones.push(1); }
-            else if stones[i].to_string().len() % 2 == 0 {
-                let left = &stones[i].to_string()[..stones[i].to_string().len() / 2];
-                let right = &stones[i].to_string()[stones[i].to_string().len() / 2..];
+    let mut stones: Vec<u128> = file.trim().split(" ").into_iter().map(|s| s.parse().unwrap()).collect();
+    for _i in 0..25 {
+        let mut new_stones: Vec<u128> = Vec::new();
+        for stone in stones {
+            if stone == 0 { new_stones.push(1); }
+            else if stone.to_string().len() % 2 == 0 {
+                let left = &stone.to_string()[..stone.to_string().len() / 2];
+                let right = &stone.to_string()[stone.to_string().len() / 2..];
                 new_stones.push(left.parse().unwrap());
                 new_stones.push(right.parse().unwrap());
             }
-            else { new_stones.push(stones[i] * 2024); }
-            i += 1;
+            else { new_stones.push(stone * 2024); }
         }
         stones = new_stones.clone();
     }
