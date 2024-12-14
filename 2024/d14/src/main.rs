7086148768ce5faf9f060a9b0d34616e5211a123
Fazekas Milán <fazekas.milan1@gmail.com>
2024-12-14 18:55:06 +0100

    2024 day 14 second  *

--- 2024/d14/src/main.rs
+++ 2024/d14/src/main.rs
@@ -6,6 +6,69 @@ struct Robot {
     vel_y: isize,
 }
 
+fn foo(robots: &mut Vec<Robot>) {
+    let tall: usize = 103;
+    let wide: usize = 101;
+    let mut c = 0;
+    loop {
+        c += 1;
+        for r in robots.iter_mut() {
+            let x = (r.x as isize + r.vel_x) % wide as isize;
+            let y = (r.y as isize + r.vel_y) % tall as isize;
+            r.x = if x < 0 {(wide as isize + x) as usize} else {x as usize};
+            r.y = if y < 0 {(tall as isize + y) as usize} else {y as usize};
+        }
+        let mut tmp = vec![vec![0; wide as usize]; tall as usize];
+        for r in robots.iter() {
+            tmp[r.y][r.x] += 1;
+        }
+        let mut good = false;
+        if c > 100 {
+            for i in 1..tmp.len() - 1 {
+                for j in 1..tmp[0].len() - 1 {
+                    if tmp[i + 1][j] != 0 && tmp[i - 1][j] != 0 && tmp[i][j + 1] != 0 && tmp[i][j - 1] != 0 && tmp[i + 1][j + 1] != 0 && tmp[i + 1][j - 1] != 0 && tmp[i - 1][j + 1] != 0 && tmp[i - 1][j - 1] != 0 { good = true; break; }
+                }
+                if good { break; }
+            }
+        }
+        if good {
+            for line in tmp {
+                for n in line {
+                    if n != 0 { print!("#"); }
+                    else { print!("."); }
+                }
+                println!();
+            }
+            break;
+        }
+        if c == 100 {
+            let mut counts = vec![0; 4];
+            for i in 0..tall / 2 {
+                for j in 0..wide / 2 {
+                    counts[0] += tmp[i][j];
+                }
+            }
+            for i in tall / 2 + 1..tall {
+                for j in 0..wide / 2 {
+                    counts[1] += tmp[i][j];
+                }
+            }
+            for i in 0..tall / 2 {
+                for j in wide / 2 + 1..wide {
+                    counts[2] += tmp[i][j];
+                }
+            }
+            for i in tall / 2 + 1..tall {
+                for j in wide / 2 + 1..wide {
+                    counts[3] += tmp[i][j];
+                }
+            }
+            println!("{}", counts[0] * counts[1] * counts[2] * counts[3]);
+        }
+    }
+    println!("{c}");
+}
+
 fn main() {
     let file = include_str!("../input2.txt");
     let mut robots: Vec<Robot> = Vec::new();
@@ -18,41 +81,6 @@ fn main() {
 
         robots.push(Robot{x, y, vel_x, vel_y});
     }
-    let tall: usize = 103;
-    let wide: usize = 101;
-    for _i in 0..100 {
-        for r in robots.iter_mut() {
-            let x = (r.x as isize + r.vel_x) % wide as isize;
-            let y = (r.y as isize + r.vel_y) % tall as isize;
-            r.x = if x < 0 {(wide as isize + x) as usize} else {x as usize};
-            r.y = if y < 0 {(tall as isize + y) as usize} else {y as usize};
-        }
-    }
 
-    let mut tmp = vec![vec![0; wide as usize]; tall as usize];
-    for r in robots {
-        tmp[r.y][r.x] += 1;
-    }
-    let mut counts = vec![0; 4];
-    for i in 0..tall / 2 {
-        for j in 0..wide / 2 {
-            counts[0] += tmp[i][j];
-        }
-    }
-    for i in tall / 2 + 1..tall {
-        for j in 0..wide / 2 {
-            counts[1] += tmp[i][j];
-        }
-    }
-    for i in 0..tall / 2 {
-        for j in wide / 2 + 1..wide {
-            counts[2] += tmp[i][j];
-        }
-    }
-    for i in tall / 2 + 1..tall {
-        for j in wide / 2 + 1..wide {
-            counts[3] += tmp[i][j];
-        }
-    }
-    println!("{}", counts[0] * counts[1] * counts[2] * counts[3]);
+    foo(&mut robots);
 }
