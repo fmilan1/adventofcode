8d361ec92032840923055f999a49864c008c99e3
Fazekas Milán <fazekas.milan1@gmail.com>
2024-12-23 18:34:23 +0100

    2024 day 23 second *

--- 2024/d23/src/main.rs
+++ 2024/d23/src/main.rs
@@ -12,7 +12,6 @@ fn bfs(graph: &BTreeMap<String, BTreeSet<String>>, start: String) -> BTreeSet<Ve
             tmp.sort();
             sets.insert(tmp);
         }
-
         for neighbor in graph[&current].iter() {
             if path.contains(&current) { continue; }
             let mut tmp = path.clone();
@@ -25,18 +24,46 @@ fn bfs(graph: &BTreeMap<String, BTreeSet<String>>, start: String) -> BTreeSet<Ve
     sets
 }
 
+fn bron_kerbosch(graph: &BTreeMap<String, BTreeSet<String>>, r: &mut BTreeSet<String>, p: &mut BTreeSet<String>, x: &mut BTreeSet<String>, cliques: &mut Vec<BTreeSet<String>>) -> Vec<BTreeSet<String>>{
+    if p.is_empty() && x.is_empty() {
+        cliques.push(r.clone());
+    }
+    for node in p.clone().iter() {
+        let mut new_r = r.clone();
+        let mut new_p = BTreeSet::new();
+        let mut new_x = BTreeSet::new();
+        new_r.insert(node.to_string());
+        for n in graph[node].iter() {
+            if p.contains(n) {
+                new_p.insert(n.to_string());
+            }
+            if x.contains(n) {
+                new_x.insert(n.to_string());
+            }
+        }
+        bron_kerbosch(graph, &mut new_r, &mut new_p, &mut new_x, cliques);
+        new_p = p.clone();
+        new_p.remove(node);
+        new_x = x.clone();
+        new_x.insert(node.to_string());
+        *p = new_p.clone();
+        *x = new_x.clone();
+    }
+    cliques.to_vec()
+}
+
 fn main() {
     let file = include_str!("../input2.txt");
-    let mut conns: BTreeMap<String, BTreeSet<String>> = BTreeMap::new();
+    let mut connections: BTreeMap<String, BTreeSet<String>> = BTreeMap::new();
     for line in file.lines() {
         let arr = line.split("-").collect::<Vec<&str>>();
-        conns.entry(arr[0].to_string()).and_modify(|set| {set.insert(arr[1].to_string());}).or_insert(BTreeSet::from([arr[1].to_string()]));
-        conns.entry(arr[1].to_string()).and_modify(|set| {set.insert(arr[0].to_string());}).or_insert(BTreeSet::from([arr[0].to_string()]));
+        connections.entry(arr[0].to_string()).and_modify(|set| {set.insert(arr[1].to_string());}).or_insert(BTreeSet::from([arr[1].to_string()]));
+        connections.entry(arr[1].to_string()).and_modify(|set| {set.insert(arr[0].to_string());}).or_insert(BTreeSet::from([arr[0].to_string()]));
     }
 
     let mut sets: BTreeSet<Vec<String>> = BTreeSet::new();
-    for pc in conns.keys().into_iter() {
-        let mut set = bfs(&conns, pc.to_string());
+    for pc in connections.keys().into_iter() {
+        let set = bfs(&connections, pc.to_string());
         for s in set.iter() {
             sets.insert(s.to_vec());
         }
@@ -53,4 +80,16 @@ fn main() {
     }
     println!("{count}");
 
+
+    let keys = connections.keys().map(|k| k.to_string()).collect::<Vec<String>>();
+    let mut set: BTreeSet<String> = BTreeSet::new();
+    for k in keys.iter() {
+        set.insert(k.to_string());
+    }
+
+    let cliques = bron_kerbosch(&connections, &mut BTreeSet::new(), &mut set, &mut BTreeSet::new(), &mut Vec::new());
+
+    let mut max_clique = cliques.iter().max_by(|a, b| a.len().cmp(&b.len())).unwrap().iter().map(|a| a.to_string()).collect::<Vec<String>>();
+    max_clique.sort();
+    println!("{}", max_clique.join(","));
 }
