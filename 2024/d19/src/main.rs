259a55e7605c940d7fc33973be1bc3cf0f2eb8d6
Fazekas Milán <fazekas.milan1@gmail.com>
2024-12-25 11:07:15 +0100

    2024 day 19 first *

--- 2024/d19/src/main.rs
+++ 2024/d19/src/main.rs
@@ -1,39 +1,44 @@
-use itertools::Itertools;
+use std::collections::{BTreeSet, BinaryHeap};
+
+fn bfs(patterns: &BTreeSet<String>, design: &String) -> bool {
+    let mut visited: BTreeSet<String> = BTreeSet::new();
+    let mut heap: BinaryHeap<(usize, String)> = BinaryHeap::new();
+    heap.push((0, String::new()));
+
+    while heap.len() > 0 {
+        let (_, path) = heap.pop().unwrap();
+
+        for pattern in patterns.iter() {
+            let new_path = path.to_owned() + pattern;
+            if visited.contains(&new_path) { continue; }
+            if new_path == *design {
+                return true;
+            }
+            if new_path.len() >= design.len() { continue; }
+            if !design.starts_with(&new_path) { continue; }
+            visited.insert(new_path.to_owned());
+            heap.push((new_path.len(), new_path));
+        }
+    }
+    false
+}
 
 fn main() {
-    let file = include_str!("../input1.txt");
-    let mut avail: Vec<String> = file.split("\n\n").nth(0).unwrap().split(", ").map(|a| a.to_string()).collect();
-    // avail.sort_by_key(|a| -(a.len() as isize));
-
-    dbg!(&avail.len());
-    for i in 0..1 {
-        for a in avail.clone().iter() {
-            avail.push(a.clone());
-        }
-    }
-    dbg!(&avail.len());
-    // let mut perms: Vec<Vec<&String>> = ;
-    // dbg!(&avail);
-
-
+    let file = include_str!("../input2.txt");
+    let patterns: Vec<String> = file.split("\n\n").nth(0).unwrap().split(", ").map(|a| a.to_string()).collect();
     let designs: Vec<String> = file.split("\n\n").nth(1).unwrap().lines().map(|a| a.to_string()).collect();
 
-    let mut possibles: Vec<String> = Vec::new();
-    for design in designs.iter() {
-        for perm in avail.iter().permutations(avail.len()).unique() {
-            // dbg!(&perm.into_iter().join(""));
-            let tmp_perm = perm.into_iter().join("");
-            if tmp_perm.contains(design) {
-                possibles.push(design.clone());
-                break;
+    let mut count = 0;
+    for  design in designs.iter() {
+        let mut new_patterns = BTreeSet::new();
+        for p in patterns.iter() {
+            if design.contains(p) {
+                new_patterns.insert(p.to_owned());
             }
         }
+        if bfs(&new_patterns, &design) {
+            count += 1;
+        }
     }
-
-    dbg!(&possibles);
-    println!("{}", possibles.len());
-
-    // 339 low
-    // 340 not good idk which way
-    // 352 not good idk which way
+    println!("{count}");
 }
