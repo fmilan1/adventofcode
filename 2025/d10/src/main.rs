9569a7c3f6aa1935ad266f89954776d95e9027a5
Fazekas Milán <fazekas.milan1@gmail.com>
2025-12-10 12:50:31 +0100

    2025 day 10 first

---
+++ 2025/d10/src/main.rs
@@ -0,0 +1,46 @@
+use std::{env::args, fs::read_to_string, usize};
+
+fn main() {
+    let file = read_to_string(args().collect::<Vec<String>>()[1].clone()).unwrap();
+    let mut sum = 0;
+    for line in file.lines() {
+        let tokens: Vec<String> = line.split(" ").map(|s| s.to_string()).collect::<Vec<String>>();
+        let mut diagram = tokens.first().unwrap().chars().map(|c| if c == '#' { true } else { false }).collect::<Vec<bool>>();
+        diagram.remove(0);
+        diagram.pop();
+        let mut buttons: Vec<Vec<usize>> = Vec::new();
+        for t in tokens.iter().skip(1).take(tokens.len() - 2) {
+            let tokens2 = t.replace("(", "").replace(")", "").split(",").map(|s| s.parse::<usize>().unwrap()).collect::<Vec<_>>();
+            buttons.push(tokens2);
+        }
+        let mut map: Vec<(usize, Vec<bool>, Vec<Vec<usize>>)> = Vec::new();
+
+        for btn in buttons.iter() {
+            let mut lights = vec![false; tokens[0].len() - 2];
+            for b in btn.iter() {
+                lights[*b] = !lights[*b];
+            }
+            map.push((1, lights, vec![btn.clone()]));
+        }
+
+        for btn in buttons.iter() {
+            for m in map.clone().iter() {
+                let (mut tmp_c, mut tmp_vec, mut tmp_btns) = m.clone();
+                for b in btn.iter() {
+                    tmp_vec[*b] = !tmp_vec[*b];
+                }
+                tmp_c += 1;
+                tmp_btns.push(btn.clone());
+                map.push((tmp_c, tmp_vec, tmp_btns));
+            }
+        }
+        map.sort_by_key(|m| m.0);
+        for m in map.iter() {
+            if m.1 == diagram {
+                sum += m.0;
+                break;
+            }
+        }
+    }
+    println!("{sum}");
+}
